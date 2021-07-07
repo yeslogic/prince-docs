589653e6c08350b6aa3cea60c902c5a201db9353
Hallvord Reiar Michaelsen Steen <hallvord@hallvord.com>
2021-07-07 16:13:18 +0200

    Working on adding info from 'annotations', still WIP

--- src/components/JavascriptSupportItem/index.js
+++ src/components/JavascriptSupportItem/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './JavascriptSupportItem.module.css';
+// some things we ignore..
+const BLOCKLIST = ['constructor', 'write', 'writeln'];
 
 function JavascriptSupportItem({open, id, name, desc, ext, subProperties, theObject, annotations}) {
-  if (['constructor', 'write', 'writeln'].includes(name)) {return null;}
-
-  console.log(subProperties)
+  if (BLOCKLIST.includes(name)) {return null;}
   open = open || location.hash.indexOf(id) > -1;
+  let theAnnotation = findAnnotation(annotations, id) || {};
+  desc = desc || theAnnotation.desc;
+  ext = ext || theAnnotation.ext || /prince/i.test(name);
+  let type = theAnnotation.type;
+  if (!type) {
+    try {
+      type = typeof theObject;
+    } catch(e) {}
+  }
   return (
     <details className={clsx('level', {ext})} open={open}>
       <summary id={id}>
@@ -16,30 +25,42 @@ function JavascriptSupportItem({open, id, name, desc, ext, subProperties, theObj
         <a href={`#${id}`} className="self-link"></a>
       </summary>
       <div>
-        <span className="type">{typeof theObject}</span>
+        <span className="type">{type}</span>
         {desc && <div className="desc">{desc}</div>}
+
       </div>
       {
         subProperties && Object.keys(subProperties).map(itemName => {
           let details = subProperties[itemName];
-          let ext = /(prince|boxinfo|pdf)/i.test(itemName);
           let subId = `${id}.${itemName}`;
-          let open = location.hash.indexOf(subId) > -1;
           return (
             <JavascriptSupportItem
               key={subId}
               id={subId}
-              open={open}
               name={itemName}
-              theObject={theObject[itemName]}
               subProperties={details}
-              ext={ext}
             />
           );
         })
       }
     </details>
   )
+}
+
+function findAnnotation(annotations, path) {
+  let item = annotations;
+  let parts = path.split(/\./g);
+  if (parts[0] === 'window') {
+    parts.shift();
+  }
+  console.log(parts, path)
+  for (let i = 0; i < parts.length; i++) {
+    console.log(parts[i], item && item[parts[i]])
+    if (parts[i] && item && item[parts[i]]) {
+      item = item[parts[i]];
+    }
+  }
+  return item === annotations ? null : item;
 }
 
 export default JavascriptSupportItem;
