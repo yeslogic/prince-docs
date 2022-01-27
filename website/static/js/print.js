8237ff09a0fdb845d1c3c749f371c3e6f2e25da1
Claudio Santambrogio <csant@csant.info>
2022-01-27 13:39:27 +0100

    Adding a ToC after the cover page in the documentation PDF.

--- website/static/js/print.js
+++ website/static/js/print.js
@@ -12,5 +12,15 @@ window.onload = function() {
                 }
             }
         }
+        if (document.location.href.indexOf('intro-userguide') > -1) {
+            var docsNav = document.getElementById('docsNav');
+            docsNav.style.setProperty('display', 'block');
+            docsNav.getElementsByClassName('navGroups')[0].style.setProperty('display', 'block');
+            docsNav.getElementsByClassName('navBreadcrumb')[0].style.setProperty('display', 'none');
+            var toc = docsNav.getElementsByClassName('navGroup');
+            toc[0].style.setProperty('display', 'none');
+            toc[1].lastChild.removeAttribute('class');
+            toc[2].lastChild.removeAttribute('class');
+        }
     }
 }
