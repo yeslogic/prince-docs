e8e46b0c6b8e9d452ef8e14df349ff3130bd829b
Claudio Santambrogio <csant@csant.info>
2023-01-09 09:03:10 +0100

    A more discrete "fix" for a Algolia search dropdown glitch.

--- website/static/js/menufix.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    var menu = document.querySelectorAll('.navGroupCategoryTitle');
-    for (var i = menu.length; i --> 0;) {
-        menu[i].innerHTML = menu[i].innerHTML.replace(' |', '');
-    }
-}
