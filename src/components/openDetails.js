ae595aa20769bc87cf219cfb352ee18c273741fb
Di <didoesdigital+git@gmail.com>
2025-09-08 19:59:16 +1000

    Add guard against invalid strings used as id

--- src/components/openDetails.js
+++ src/components/openDetails.js
@@ -2,16 +2,20 @@ export function openDetails() {
   const hash = window.location.hash;
   if (hash) {
     const id = hash.substring(1); // Remove the '#' from the hash
-    const element = document.getElementById(id);
-    if (element) {
-      let parentDetails = element.parentNode;
-      while (parentDetails && parentDetails.tagName !== "DETAILS") {
-        parentDetails = parentDetails.parentNode;
+    try {
+      const element = document.getElementById(id);
+      if (element) {
+        let parentDetails = element.parentNode;
+        while (parentDetails && parentDetails.tagName !== "DETAILS") {
+          parentDetails = parentDetails.parentNode;
+        }
+        if (parentDetails) {
+          parentDetails.setAttribute("open", "");
+          element.scrollIntoView(); // This is necessary to scroll to IDs inside collapsed `details` elements over a slow connection
+        }
       }
-      if (parentDetails) {
-        parentDetails.setAttribute("open", "");
-        element.scrollIntoView(); // This is necessary to scroll to IDs inside collapsed `details` elements over a slow connection
-      }
+    } catch (e) {
+      console.error(e);
     }
   }
 }
