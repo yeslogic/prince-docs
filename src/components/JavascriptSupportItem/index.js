c4dba2884eb0e21534f702f58ff97f38be81935b
Di <didoesdigital+git@gmail.com>
2025-09-08 14:47:01 +1000

    Update build of JS Support page
    - Add Makefile for js-support page to combine std.json and
      std-annotated.json into _std-data.json using jq
    - Remove BrowserOnly and client-side behaviour in favour of using type
      information in __princetype__ key from _std-data.json
    - Move "See the property " from CSS pseudo elements into JSX/HTML
    - Move js-support.md content into JS to add <span className="dep">

--- src/components/JavascriptSupportItem/index.js
+++ src/components/JavascriptSupportItem/index.js
@@ -1,143 +1,265 @@
 import React from "react";
 import clsx from "clsx";
 import styles from "./JavascriptSupportItem.module.css";
+
 // some things we ignore..
-const BLOCKLIST = ["constructor", "write", "writeln", "prototype"];
+const BLOCKLIST = ["constructor", "write", "writeln"];
+
+const generatedFields = ["name", "__princetype__"];
+const handWrittenAnnotations = [
+  "desc",
+  "arguments",
+  "name", // Hand-written on arguments objects
+  "desc", // Hand-written on arguments objects
+  "type", // Hand-written on arguments objects
+  "returns",
+  "example",
+  "exampleReturn",
+  "url",
+  "ext",
+  "dep"
+];
+const ignoreFields = [
+  ...new Set([...generatedFields, ...handWrittenAnnotations])
+];
 
 // We use three sources of information:
-// - a data structure dumped from the engine listing what properties/objects/methods are supported
-// - a data structure similar to the above but annotated with human comments and even short examples
-//   (we should aim to make sure the Prince-specific parts in particular are described)
-// - the engine this script runs in is queried directly for type info
+// - a data structure dumped from Prince listing what properties/objects/
+//   methods are supported and their types (`__princetype__`)
+// - a data structure similar to the above but with type information for
+//   arguments and properties annotated with human comments and even short
+//   examples and (we should aim to make sure the Prince-specific parts in
+//   particular are described)
 //
 // Finally, the script also infers some details from item names, and can infer more
 
+function NameDetails({ name, children }) {
+  return (
+    <code>
+      <b className="name">
+        {name}
+        {children}
+      </b>
+    </code>
+  );
+}
+
+// { princetype : null | "string" }
+function TypeDetails({ princetype }) {
+  return princetype === null ? (
+    // Instead of `-`, we could possibly return `null` here so it does not render this span at all:
+    <span className="type"></span>
+  ) : (
+    <span className="type">{princetype}</span>
+  );
+}
+
+function UrlDetails({ property }) {
+  const cssProperty =
+    property === "cssFloat"
+      ? "float"
+      : property.replace(/([A-Z])/g, "-$1").toLowerCase();
+  const cssPropertyLabel =
+    property.indexOf("prince") > -1 ? `-${cssProperty}` : cssProperty;
+
+  return (
+    <div className="url-property">
+      See the property{" "}
+      <a href={`/doc/css-props#prop-${cssProperty}`}>{cssPropertyLabel}</a>.
+    </div>
+  );
+}
+
+// { desc: undefined | string, name: string } - if desc is a string, it may contain HTML tags or just a string
+function DescDetails({ desc, name }) {
+  const showDesc = desc || /^HTML/.test(name);
+  if (!showDesc) return null;
+
+  return desc ? (
+    <div className="desc" dangerouslySetInnerHTML={{ __html: desc }}></div>
+  ) : (
+    <div className="desc">
+      Represents an HTML{" "}
+      <em>
+        {name
+          .replace(/^HTML/, "")
+          .replace(/Element$/, "")
+          .replace(/([A-Z][a-z])/g, function (match) {
+            return " " + match;
+          })
+          .toLowerCase()}
+      </em>{" "}
+      element
+    </div>
+  );
+}
+
+// undefined | { type: string, name: string, desc?: string }[]
+function ArgsList({ args }) {
+  // Note: if args is undefined (e.g. for parseFloat()), then we just add the span to make "()" appear
+  return (
+    <span className="argslist">
+      {args ? args.map((item) => item.name).join(", ") : null}
+    </span>
+  );
+}
+
+// undefined | { type: string, name?: string, desc?: string }[]
+function ArgsDetails({ args }) {
+  return args ? (
+    <ul className="arguments">
+      {args.map((item, i) => (
+        <li className="argument" key={`${item.name}-${i}`}>
+          <div></div>
+          {item.name && <div className="name level">{item.name}</div>}
+          <div className="type">{item.type}</div>
+          {item.desc && <div className="desc">{item.desc}</div>}
+        </li>
+      ))}
+    </ul>
+  ) : null;
+}
+
+// { returns: string } - may contain HTML tags or just string
+function ReturnsDetails({ returns }) {
+  return (
+    <div
+      className="returns level"
+      dangerouslySetInnerHTML={{ __html: returns }}
+    ></div>
+  );
+}
+
+// { example: string, exampleReturn: undefined | string | number | boolean } - example is a string that may contain new lines (\n) and exampleReturn is either undefined or JSON data that currently only contains number or boolean but could contain a string
+function ExampleDetails({ example, exampleReturn }) {
+  return (
+    <div className="example">
+      <div className="programlisting">
+        <pre className="example level">
+          {example}
+          {exampleReturn && (
+            <div className="example-return">
+              {JSON.stringify(exampleReturn)}
+            </div>
+          )}
+        </pre>
+      </div>
+    </div>
+  );
+}
+
 function JavascriptSupportItem({
-  name,
+  // {string[]} - e.g. [ "window", "BoxInfo", "prototype", "marginTop" ]
   path,
+  // {string} - e.g. "marginBottom", "prototype", "NaN", "undefined", "eval", "Prince", "PDF"
+  name,
+  // {null | undefined | string | Object} - e.g. undefined, "function", "object", { type: "string"}, { type: "function", returns: '"BODY" | "COLUMN" | "FLEXLINE" | "FOOTNOTES" | "FLOATS" | "BOX" | "LINE" | "SPAN" | "TEXT" | "SVG" | "IMAGE"' }
+  princetype,
+  // {undefined | string} - e.g. "The global object", "See <a href='/doc/javascript#the-prince-object'>The Prince Object</a>."
   desc,
+  // {null | [itemName, {<property>: string}}[]} - each property might be a generated field, an annotation, or an actual property of the JavaScript feature
+  properties,
+  // {undefined | { type: string, name: string, desc?: string }[]} - e.g. [{ type: "boolean" }], [{ name: "string", desc: "String to unescape", type: "string" }]
+  args,
+  // {undefined | string} - e.g. "boolean", "number", "function", "string", "array of child boxes", "A list of JavaScript objects called <a href='/doc/javascript#the-box-tracking-api'>boxes</a>."
+  returns,
+  // {undefined | string} - e.g. "isNaN(parseInt('hello'))"
+  example,
+  // {undefined | string | boolean | number} - e.g. `1.5`, `50`, `true`
+  exampleReturn,
+  // {undefined | string} - e.g. "property" where there is a matching CSS property
+  url,
+  // {undefined | string} - e.g. "ext" for Prince extensions
   ext,
-  open,
-  dumpedPropertyList,
-  uaObject,
-  annotations,
+  // {undefined | string} - e.g. "dep" for deprecated objects, methods and properties
+  dep,
+  // {undefined | string} - just the URL hash without "#" e.g. "window.Object.defineProperty"
+  hash,
+  // {undefined | boolean} - set `true` to open the section by default
+  open
 }) {
   if (BLOCKLIST.includes(name)) {
     return null;
   }
-  let pathStr = path.join(".");
-  open = open || location.hash.indexOf(pathStr) > -1;
-  let theAnnotation = findAnnotation(annotations, path) || {};
-  desc = desc || theAnnotation.desc;
 
-  if (!desc && /^HTML/.test(name)) {
-    desc =
-      "Represents an HTML <em>" +
-      name
-        .replace(/^HTML/, "")
-        .replace(/Element$/, "")
-        .replace(/([A-Z][a-z])/g, function (match) {
-          return " " + match;
-        })
-        .toLowerCase() +
-      "</em> element";
-  }
-
-  ext = ext || theAnnotation.ext || /prince/i.test(name) ? "ext" : null;
-  let type = theAnnotation.type;
-  if (uaObject) {
-    try {
-      if (!type) {
-        type = typeof uaObject;
-      }
-    } catch (e) {}
-  }
+  const pathStr = path.join(".");
+  const openAttribute = open || (hash && hash.indexOf(pathStr) > -1);
+  const extClass = ext || /prince/i.test(name) ? "ext" : null;
+  const depClass = dep ? "dep" : null;
+  const isFunctionProperty = princetype === "function" || args;
 
   return (
-    <details className={clsx("level", { ext })} open={open}>
+    <details
+      className={clsx("level", { ext: extClass, dep: depClass })}
+      open={openAttribute}
+    >
       <summary id={pathStr}>
-          <code><b className="name">{name}</b></code>
-          {(type === "function" || theAnnotation.arguments) && (
-            <span className="argslist">
-              {theAnnotation.arguments
-                ? theAnnotation.arguments.map((item) => item.name).join(", ")
-                : null}
-            </span>
-          )}
+        <NameDetails name={name}>
+          {isFunctionProperty && <ArgsList args={args} />}
+        </NameDetails>
         <a href={`#${pathStr}`} className="hash-link"></a>
       </summary>
       <div>
-        <span className="type">{type}</span>
-        {desc && (
-          <div
-            className="desc"
-            dangerouslySetInnerHTML={{ __html: desc }}
-          ></div>
-        )}
-        {theAnnotation.arguments && (
-          <ul className="arguments">
-            {theAnnotation.arguments.map((item) => (
-              <li className="argument">
-                <div></div>
-                <div className="name level">{item.name}</div>
-                <div className="type">{item.type}</div>
-                <div className="desc">{item.desc}</div>
-              </li>
-            ))}
-          </ul>
-        )}
-        {theAnnotation.returns && (
-          <div className="returns level" dangerouslySetInnerHTML={{__html:theAnnotation.returns}}></div>
-        )}
-
-        {theAnnotation.example && (
-          <div className="example">
-            <div className="programlisting">
-              <pre className="example level">
-                {theAnnotation.example}
-                {theAnnotation.exampleReturn && (
-                  <div className="example-return">
-                    {JSON.stringify(theAnnotation.exampleReturn)}
-                  </div>
-                )}
-              </pre>
-            </div>
-          </div>
+        <TypeDetails princetype={princetype} />
+        {url && <UrlDetails property={name} />}
+        <DescDetails desc={desc} name={name} />
+        <ArgsDetails args={args} />
+        {returns && <ReturnsDetails returns={returns} />}
+        {example && (
+          <ExampleDetails example={example} exampleReturn={exampleReturn} />
         )}
       </div>
-      {dumpedPropertyList &&
-        Object.keys(dumpedPropertyList).map((itemName) => {
-          let subPropertyList = dumpedPropertyList[itemName];
+      {!!properties &&
+        properties.map(([itemName, subProperties]) => {
+          let subPropertyList = Object.entries(subProperties).filter(
+            (subProperty) => {
+              if (
+                subProperty[0] === "type" &&
+                typeof subProperty[1] !== "string"
+              ) {
+                // For example, HTMLInputElement's `type` property:
+                return true;
+              }
+              if (
+                subProperty[0] === "name" &&
+                typeof subProperty[1] !== "string"
+              ) {
+                // For example, HTMLInputElement's `name` property:
+                return true;
+              }
+
+              const isIgnoredField = ignoreFields.includes(subProperty[0]);
+              return !isIgnoredField;
+            }
+          );
           let subPath = path.concat(itemName);
-          let theSubObject = null;
-          try {
-            theSubObject = uaObject[itemName];
-          } catch (e) {}
+
+          // Skip args if subProperties.arguments is the Function.prototype.arguments object:
+          const subPropertiesArgs = Array.isArray(subProperties.arguments)
+            ? subProperties.arguments
+            : undefined;
 
           return (
             <JavascriptSupportItem
               key={subPath.join(".")}
               path={subPath}
               name={itemName}
-              dumpedPropertyList={subPropertyList}
-              uaObject={theSubObject}
-              annotations={annotations}
+              princetype={subProperties.__princetype__}
+              desc={subProperties.desc}
+              args={subPropertiesArgs}
+              returns={subProperties.returns}
+              example={subProperties.example}
+              exampleReturn={subProperties.exampleReturn}
+              url={subProperties.url}
+              ext={subProperties.ext}
+              dep={subProperties.dep}
+              properties={subPropertyList.length < 1 ? null : subPropertyList}
+              hash={hash}
             />
           );
         })}
     </details>
   );
-}
-
-function findAnnotation(annotations, path) {
-  let item = annotations;
-  let i = path[0] === "window" ? 1 : 0;
-  for (; i < path.length; i++) {
-    if (path[i] && item && item[path[i]]) {
-      item = item[path[i]];
-    }
-  }
-  return item === annotations ? null : item;
 }
 
 export default JavascriptSupportItem;
