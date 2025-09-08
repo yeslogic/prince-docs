2ea17f310d549e7c8a5005caf286c8804c98eaa5
Di <didoesdigital+git@gmail.com>
2025-09-08 14:57:19 +1000

    Remove unused style file

--- src/components/JavascriptSupportItem/index.js
+++ src/components/JavascriptSupportItem/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import clsx from "clsx";
-import styles from "./JavascriptSupportItem.module.css";
 
 // some things we ignore..
 const BLOCKLIST = ["constructor", "write", "writeln"];
