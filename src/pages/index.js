182b2097862fc6e3a0bd62e919b9e873ac30b29a
Claudio Santambrogio <csant@csant.info>
2025-10-03 17:06:03 +0200

    Removing unnecessary imports.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
